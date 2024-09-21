b590a08855e8307a9c6d4b81a095603acb2c3c9f
Victor Emanuel <victoremanuel.jittechnology@gmail.com>
2024-09-21 13:42:30 -0300

    Add NotFound.vue page

---
+++ frontend/projeto-final-web/src/pages/Admin/LoginPage.vue
@@ -0,0 +1,115 @@
+<script setup lang="js">
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { api } from '@/api/'
+import { useUserStore } from '@/stores/userStore'
+import { isAxiosError } from 'axios'
+import { isApplicationError } from '@/composables/useApplicationError.js'
+
+const identifier = ref('')
+const password = ref('')
+const loading = ref(false)
+const exception = ref(undefined)
+const router = useRouter()
+const userStore = useUserStore()
+
+// Para controlar a validação
+const formSubmitted = ref(false)
+
+// Função de validação simples de e-mail: deve conter "@" seguido de um "."
+function isValidEmail(email) {
+  return email.includes('@') && email.split('@')[1]?.includes('.')
+}
+
+async function authenticate() {
+  formSubmitted.value = true
+  if (!isValidEmail(identifier.value) || !password.value) {
+    return
+  }
+
+  try {
+    loading.value = true
+    exception.value = undefined
+    const { data } = await api.post('/auth/local', {
+      identifier: identifier.value,
+      password: password.value
+    })
+    const { jwt } = data
+
+    const res = await api.get('/users/me', {
+      headers: {
+        Authorization: `Bearer ${jwt}`
+      },
+      params: {
+        populate: 'role'
+      }
+    })
+
+    const role = res.data.role.type
+
+    userStore.authenticaded(res.data, jwt)
+
+    if (role === 'admin') {
+      router.push('/admin')
+    } else {
+      router.push('/')
+    }
+  } catch (e) {
+    if (isAxiosError(e) && isApplicationError(e.response?.data)) {
+      exception.value = e.response?.data
+    }
+  } finally {
+    loading.value = false
+  }
+}
+</script>
+
+<template>
+  <div class="row justify-content-center">
+    <div class="col-6 card">
+      <div class="card-body">
+        <h5 class="card-title">Sign in</h5>
+        <div v-if="exception" class="alert alert-danger" role="alert">
+          {{ exception.error.message }}
+        </div>
+        <div v-if="loading" class="spinner-grow" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+        <form v-else @submit.prevent="authenticate" novalidate>
+          <div class="mb-3">
+            <label for="emailInput" class="form-label">Email:</label>
+            <input
+              type="email"
+              class="form-control"
+              id="emailInput"
+              placeholder="mail@mail.com"
+              v-model="identifier"
+              :class="{ 'is-invalid': formSubmitted && !isValidEmail(identifier) }"
+              required
+            />
+            <div v-if="formSubmitted && !isValidEmail(identifier)" class="invalid-feedback">
+              Você deve informar um email válido.
+            </div>
+          </div>
+          <div class="mb-3">
+            <label for="passwordInput" class="form-label">Password</label>
+            <input
+              type="password"
+              class="form-control"
+              id="passwordInput"
+              v-model="password"
+              :class="{ 'is-invalid': formSubmitted && !password }"
+              required
+            />
+            <div v-if="formSubmitted && !password" class="invalid-feedback">
+              A senha é um campo obrigatório.
+            </div>
+          </div>
+          <div class="mb-3">
+            <input type="submit" class="float-end btn btn-primary" value="Enviar" />
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
